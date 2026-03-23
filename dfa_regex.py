21c01036d7bab4326c5494485f197b6250119fb4
Juaneliz <A01708389@tec.mx>
2026-03-23 16:29:56 -0600

    Update dfa_regex.py

--- dfa_regex.py
+++ dfa_regex.py
@@ -11,17 +11,13 @@ import re
 
 PATTERN = r'^(b|abb|a*(1b)*1bb)*(a+b1(1|a)*((1|a)*b+a)+|a*1+(b1)*ba((1|a)*ba)*)$'
 
-def parse(string):
-
-    return string.strip()
-
 def accept(string):
+    string = string.strip()
     return bool(re.match(PATTERN, string))
 
 if __name__ == '__main__':
     inp = str(input())
-    res = parse(inp)
-    res = accept(res)
+    res = accept(inp)
     if res:
         print("yes")
     else:
